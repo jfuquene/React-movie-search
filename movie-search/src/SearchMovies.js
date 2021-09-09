dfd6b20b4d0c210a3d0f13cde8ec70df8a80bfe3
John Fuquene <jfuquene@email.wm.edu>
2021-09-09 17:33:44 -0400

    fetch

--- movie-search/src/SearchMovies.js
+++ movie-search/src/SearchMovies.js
@@ -1,13 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
+import './index.css';
 
 
 function SearchMovies(){
 
+
+    const searchMovies = async (e) => {
+        e.preventDefault()
+
+        console.log("i am submitted")
+
+        const query = "Jurassic Park"
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=e9d64bbce5dbc782a6764a16fa7d902c&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data.results)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+
     return(
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label htmlFor="query" className="label">Search Movie:</label>
-            <input type="text" name="query" placeholder="e.i Teminator" />
-            <button type="submit" className="button"></button>
+            <input type="text" name="query" className="input"  placeholder="e.i Teminator" />
+            <button type="submit" className="button">Search</button>
     </form>
     )
 }
